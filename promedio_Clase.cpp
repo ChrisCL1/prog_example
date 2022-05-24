952ce596bbafb0fc4e605691cf857ec03ef40522
Christopher Cumi <2109048@upy.edu.mx>
2022-05-24 12:09:17 -0500

    Prueba token

--- promedio_Clase.cpp
+++ promedio_Clase.cpp
@@ -2,32 +2,32 @@
 using namespace std;
 
 int main(){
-	int fila, columna;
-	float promedio = 0.0f;
+	int row, column;
+	float average = 0.0f;
 	
-	cout<<"Introduzca el numero de alumnos: ";
-	cin>>fila;
+	cout<<"Enter the number of students: ";
+	cin>>row;
 	
-	cout<<"Introduzca el numero de calificaciones: ";
-	cin>>columna;
+	cout<<"Enter the number of grades: ";
+	cin>>column;
 	
-	float matriz[fila][columna + 1] = {0.0f};
+	float matriz[row][column + 1] = {0.0f};
 	
-	for(int i = 0; i < fila; i++){
-		cout<<"\nAlumno "<<i + 1<<":"<<endl;
-		for(int j = 0; j < columna; j++){
-			cout<<"Calificacion "<<j + 1<<":"<<endl;
+	for(int i = 0; i < row; i++){
+		cout<<"\nStudent "<<i + 1<<":"<<endl;
+		for(int j = 0; j < column; j++){
+			cout<<"Grade "<<j + 1<<":"<<endl;
 			cin>>matriz[i][j];
-			matriz[i][columna] += matriz[i][j];
-			promedio += matriz[i][j];
+			matriz[i][column] += matriz[i][j];
+			average += matriz[i][j];
 		}
 	}
 	
-	for(int i = 0; i < fila; i++){
-		cout<<"\nAlumno "<<i + 1<<": "<<"Promedio: "<<matriz[i][columna] / columna;
+	for(int i = 0; i < row; i++){
+		cout<<"\nStudent "<<i + 1<<": "<<"Average: "<<matriz[i][column] / column;
 	}
 	
-	cout<<"\nPromedio de la clase: "<<promedio / (fila * columna)<<endl;
+	cout<<"\nAverage of the class: "<<average / (row * column)<<endl;
 			
 	return 0;
 }
